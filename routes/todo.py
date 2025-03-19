0e06b065c1994018d6a7d9df3752fcfc0fb6ab04
uzevie George <uzeviegeorge@gmail.com>
2025-03-19 20:52:23 +0100

    added some exceptions and status codes

--- routes/todo.py
+++ routes/todo.py
@@ -1,31 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from schemas import todo as todo_schema
 from crud.todo import todo_crud
 
 todo_router = APIRouter(prefix="/todos", tags=["todos"])
 
 
-@todo_router.get("/")
+@todo_router.get("/", status_code=status.HTTP_200_OK)
 def get_all_todos():
     all_todos = todo_crud.get_all_todos()
     return all_todos
 
 
-@todo_router.get("/{id}", response_model=todo_schema.Todo)
+@todo_router.get("/{id}", response_model=todo_schema.Todo, status_code=status.HTTP_200_OK)
 def get_a_todo(id: str):
-    return todo_crud.get_a_todo(id)
+    todo = todo_crud.get_a_todo(id)
+    if not todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    return todo
 
-
-@todo_router.post("/", response_model=todo_schema.TodoCreate)
+@todo_router.post("/", response_model=todo_schema.TodoCreate, status_code=status.HTTP_201_CREATED)
 def make_a_todo(todo: todo_schema.TodoCreate):
     return todo_crud.create_todo(todo)
 
 
-@todo_router.put("/{id}")
+@todo_router.put("/{id}", status_code=status.HTTP_200_OK)
 def update_a_todos(id: str, todo_data: todo_schema.TodoUpdate):
-    return todo_crud.update_todo(id, todo_data)
+    updated_todo = todo_crud.update_todo(id, todo_data)
+    if not updated_todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    return updated_todo
 
 
-@todo_router.delete("/{id}")
+@todo_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_a_todos(id: str):
     return todo_crud.delete_todo(id)
--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from schemas import users as user_schema
 from crud.users import user_crud
 
@@ -6,30 +6,36 @@ from crud.users import user_crud
 user_router = APIRouter(prefix="/users", tags=["users"])
 
 
-@user_router.get("/")
+@user_router.get("/", status_code=status.HTTP_200_OK)
 def get_all_users():
     all_users = user_crud.get_all_users()
     return all_users
 
 
-@user_router.get("/{id}")
+@user_router.get("/{id}", status_code=status.HTTP_200_OK)
 def get_a_user(id: str):
-    return user_crud.get_user(id)
+    user = user_crud.get_user(id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    return user
    
 
 
-@user_router.post("/")
+@user_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_a_user(user: user_schema.UserCreate):
     return user_crud.create_user(user)
 
 
 
-@user_router.put("/{id}")
+@user_router.put("/{id}", status_code=status.HTTP_200_OK)
 def update_a_user(id: str, user_data: user_schema.UserUpdate):
-    return user_crud.update_user(id, user_data)
+    updated_user = user_crud.update_user(id, user_data)
+    if not updated_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    return updated_user
 
 
-@user_router.delete("/{id}")
+@user_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_a_user(id: str):
     return user_crud.delete_user(id)
 
